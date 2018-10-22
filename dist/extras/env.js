6f9a25cb995b34ae3bb074e58d32b3de5dec4d0c
xamantra <xamantrx@gmail.com>
2018-10-22 09:20:13 +0800

    JUMPED TO HYPERSPACE

---
+++ dist/extras/env.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class EnvironmentVariables {
+}
+EnvironmentVariables.token = "bareWithmeThisisArealTokenforAbot.IhackedDiscord.UsingmyMind";
+EnvironmentVariables.prefix = "-";
+exports.EnvironmentVariables = EnvironmentVariables;
+//# sourceMappingURL=env.js.map
