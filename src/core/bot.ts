dd07fd34ecc580d7c221b2ac2ae65b7bd5bdab08
xamantra <xamantrx@gmail.com>
2018-10-24 06:59:54 +0800

    RIKIMARU

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,8 +1,16 @@
+import * as http from "http";
+
 export class Bot {
   private static Active = false;
 
   public static Init() {
     this.Active = true;
+    http
+      .createServer(function(req, res) {
+        res.write("Hello! I am Rikimaru!");
+        res.end();
+      })
+      .listen(8080);
   }
 
   public static SetActive(
