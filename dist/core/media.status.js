43f95312ad25115e48d0c626c11f8d66e65f8096
xamantra <xamantrx@gmail.com>
2018-10-27 09:06:07 +0800

    I COULDN'T ANTICIPATE IT

--- dist/core/media.status.js
+++ dist/core/media.status.js
@@ -2,13 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class MediaStatus {
     static Ongoing($m) {
-        if ($m.status === "RELEASING" && $m.nextAiringEpisode !== null) {
+        if ($m.status === "RELEASING" &&
+            $m.nextAiringEpisode !== null &&
+            $m.nextAiringEpisode.airingAt) {
             return true;
         }
         return false;
     }
     static NotYetAired($m) {
-        if ($m.status === "NOT_YET_RELEASED" && $m.nextAiringEpisode !== null && $m.nextAiringEpisode.airingAt) {
+        if ($m.status === "NOT_YET_RELEASED" &&
+            $m.nextAiringEpisode !== null &&
+            $m.nextAiringEpisode.airingAt) {
             return true;
         }
         return false;
--- src/core/media.status.ts
+++ src/core/media.status.ts
@@ -2,14 +2,22 @@ import { IMedia } from "./../interfaces/page.interface";
 
 export class MediaStatus {
   public static Ongoing($m: IMedia) {
-    if ($m.status === "RELEASING" && $m.nextAiringEpisode !== null) {
+    if (
+      $m.status === "RELEASING" &&
+      $m.nextAiringEpisode !== null &&
+      $m.nextAiringEpisode.airingAt
+    ) {
       return true;
     }
     return false;
   }
 
   public static NotYetAired($m: IMedia) {
-    if ($m.status === "NOT_YET_RELEASED" && $m.nextAiringEpisode !== null && $m.nextAiringEpisode.airingAt) {
+    if (
+      $m.status === "NOT_YET_RELEASED" &&
+      $m.nextAiringEpisode !== null &&
+      $m.nextAiringEpisode.airingAt
+    ) {
       return true;
     }
     return false;
