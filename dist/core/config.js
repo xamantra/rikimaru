3557a3d603d373b27360c8bcd0f53481500b60f8
xamantra <xamantrx@gmail.com>
2018-10-27 08:13:19 +0800

    THIRD TRY

--- dist/core/config.js
+++ dist/core/config.js
@@ -1,34 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const env_1 = require("../extras/env");
 class Config {
     static get BOT_TOKEN() {
-        return process.env.BOT_TOKEN || env_1.env.BOT_TOKEN;
+        return process.env.BOT_TOKEN || require("../extras/env").BOT_TOKEN;
     }
     static get COMMAND_PREFIX() {
-        return process.env.COMMAND_PREFIX || env_1.env.COMMAND_PREFIX;
+        return (process.env.COMMAND_PREFIX || require("../extras/env").COMMAND_PREFIX);
     }
     static get MYSQL_HOST() {
-        return process.env.MYSQL_HOST || env_1.env.MYSQL_HOST;
+        return process.env.MYSQL_HOST || require("../extras/env").MYSQL_HOST;
     }
     static get MYSQL_PORT() {
-        return Number(process.env.MYSQL_PORT) || env_1.env.MYSQL_PORT;
+        return (Number(process.env.MYSQL_PORT) || require("../extras/env").MYSQL_PORT);
     }
     static get MYSQL_USERNAME() {
-        return process.env.MYSQL_USERNAME || env_1.env.MYSQL_USERNAME;
+        return (process.env.MYSQL_USERNAME || require("../extras/env").MYSQL_USERNAME);
     }
     static get MYSQL_PASSWORD() {
-        return process.env.MYSQL_PASSWORD || env_1.env.MYSQL_PASSWORD;
+        return (process.env.MYSQL_PASSWORD || require("../extras/env").MYSQL_PASSWORD);
     }
     static get MYSQL_DATABASE() {
-        return process.env.MYSQL_DATABASE || env_1.env.MYSQL_DATABASE;
+        return (process.env.MYSQL_DATABASE || require("../extras/env").MYSQL_DATABASE);
     }
     static get MYSQL_TIMEOUT() {
-        return Number(process.env.MYSQL_TIMEOUT) || env_1.env.MYSQL_TIMEOUT;
+        return (Number(process.env.MYSQL_TIMEOUT) ||
+            require("../extras/env").MYSQL_TIMEOUT);
     }
     static get MYSQL_CONNECTION_TIMEOUT() {
         return (Number(process.env.MYSQL_CONNECTION_TIMEOUT) ||
-            env_1.env.MYSQL_CONNECTION_TIMEOUT);
+            require("../extras/env").MYSQL_CONNECTION_TIMEOUT);
     }
 }
 exports.Config = Config;
--- src/core/config.ts
+++ src/core/config.ts
@@ -1,42 +1,53 @@
-import { env } from "../extras/env";
-
 export class Config {
   public static get BOT_TOKEN(): string {
-    return process.env.BOT_TOKEN || env.BOT_TOKEN;
+    return process.env.BOT_TOKEN || require("../extras/env").BOT_TOKEN;
   }
 
   public static get COMMAND_PREFIX(): string {
-    return process.env.COMMAND_PREFIX || env.COMMAND_PREFIX;
+    return (
+      process.env.COMMAND_PREFIX || require("../extras/env").COMMAND_PREFIX
+    );
   }
 
   public static get MYSQL_HOST(): string {
-    return process.env.MYSQL_HOST || env.MYSQL_HOST;
+    return process.env.MYSQL_HOST || require("../extras/env").MYSQL_HOST;
   }
 
   public static get MYSQL_PORT(): number {
-    return Number(process.env.MYSQL_PORT) || env.MYSQL_PORT;
+    return (
+      Number(process.env.MYSQL_PORT) || require("../extras/env").MYSQL_PORT
+    );
   }
 
   public static get MYSQL_USERNAME(): string {
-    return process.env.MYSQL_USERNAME || env.MYSQL_USERNAME;
+    return (
+      process.env.MYSQL_USERNAME || require("../extras/env").MYSQL_USERNAME
+    );
   }
 
   public static get MYSQL_PASSWORD(): string {
-    return process.env.MYSQL_PASSWORD || env.MYSQL_PASSWORD;
+    return (
+      process.env.MYSQL_PASSWORD || require("../extras/env").MYSQL_PASSWORD
+    );
   }
 
   public static get MYSQL_DATABASE(): string {
-    return process.env.MYSQL_DATABASE || env.MYSQL_DATABASE;
+    return (
+      process.env.MYSQL_DATABASE || require("../extras/env").MYSQL_DATABASE
+    );
   }
 
   public static get MYSQL_TIMEOUT(): number {
-    return Number(process.env.MYSQL_TIMEOUT) || env.MYSQL_TIMEOUT;
+    return (
+      Number(process.env.MYSQL_TIMEOUT) ||
+      require("../extras/env").MYSQL_TIMEOUT
+    );
   }
 
   public static get MYSQL_CONNECTION_TIMEOUT(): number {
     return (
       Number(process.env.MYSQL_CONNECTION_TIMEOUT) ||
-      env.MYSQL_CONNECTION_TIMEOUT
+      require("../extras/env").MYSQL_CONNECTION_TIMEOUT
     );
   }
 }
