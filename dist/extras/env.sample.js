e29abfd51d377c293e6d02f14eaa101d7e697833
xamantra <xamantrx@gmail.com>
2018-10-28 14:29:02 +0800

    LIKE A BOSS

--- dist/extras/env.sample.js
+++ dist/extras/env.sample.js
@@ -1,8 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class EnvironmentVariables {
+// tslint:disable-next-line:class-name
+class env {
 }
-EnvironmentVariables.token = "bareWithmeThisisArealTokenforAbot.IhackedDiscord.UsingmyMind";
-EnvironmentVariables.prefix = "#";
-exports.EnvironmentVariables = EnvironmentVariables;
+env.BOT_TOKEN = "ThisisAbotToken.bareWithme.IhackedDiscord.aWhileagoLikeaBoss";
+env.COMMAND_PREFIX = "-";
+env.MYSQL_HOST = "localhost";
+env.MYSQL_PORT = 3306;
+env.MYSQL_USERNAME = "root";
+env.MYSQL_PASSWORD = "";
+env.MYSQL_DATABASE = "rikimaru";
+env.MYSQL_TIMEOUT = 60000;
+env.MYSQL_CONNECTION_TIMEOUT = 60000;
+env.MYSQL_CONNECTION_LIMIT = 10;
+exports.env = env;
 //# sourceMappingURL=env.sample.js.map
--- src/extras/env.sample.ts
+++ src/extras/env.sample.ts
@@ -1,5 +1,14 @@
-export class EnvironmentVariables {
-  public static token =
-    "bareWithmeThisisArealTokenforAbot.IhackedDiscord.UsingmyMind";
-  public static prefix = "#";
+// tslint:disable-next-line:class-name
+export class env {
+  public static BOT_TOKEN =
+    "ThisisAbotToken.bareWithme.IhackedDiscord.aWhileagoLikeaBoss";
+  public static COMMAND_PREFIX = "-";
+  public static MYSQL_HOST = "localhost";
+  public static MYSQL_PORT = 3306;
+  public static MYSQL_USERNAME = "root";
+  public static MYSQL_PASSWORD = "";
+  public static MYSQL_DATABASE = "rikimaru";
+  public static MYSQL_TIMEOUT = 60000;
+  public static MYSQL_CONNECTION_TIMEOUT = 60000;
+  public static MYSQL_CONNECTION_LIMIT = 10;
 }
