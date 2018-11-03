cb9dcb05703b2b7d4e23cfb258269f4befff5c10
xamantra <xamantrx@gmail.com>
2018-11-03 18:30:38 +0800

    Updated **env.sample.ts**

--- src/extras/env.sample.ts
+++ src/extras/env.sample.ts
@@ -11,4 +11,13 @@ export class env {
   public static MYSQL_TIMEOUT = 60000;
   public static MYSQL_CONNECTION_TIMEOUT = 60000;
   public static MYSQL_CONNECTION_LIMIT = 10;
+  public static MONGO_URL = "mongodb://localhost:27017/rikimaru";
+  public static MONGO_BASE = "mongodb://localhost:27017/";
+  public static MONGO_USERNAME = "";
+  public static MONGO_PASSWORD = "";
+  public static MONGO_DATABASE = "rikimaru";
+  public static JIKAN_BASE_URL = "https://api.jikan.moe/v3/";
+  public static MAL_BASE_URL = "https://myanimelist.net/";
+  public static MAL_PROFILE_BASE = "https://myanimelist.net/profile/";
+  public static MAL_CW_BASE = "https://myanimelist.net/animelist";
 }
