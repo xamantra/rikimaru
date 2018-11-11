3d1035cb64b02a85a3050d9c4fe3e2d18adcae64
xamantra <xamantrx@gmail.com>
2018-11-11 23:36:31 +0800

    **ignored commands on dm.**

--- src/handlers/message.handler.ts
+++ src/handlers/message.handler.ts
@@ -3,6 +3,7 @@ import { Command } from "../models/command.model";
 import { ClientManager } from "../core/client";
 import { Config } from "../core/config";
 import { ResponseHandler } from "./response.handler";
+import { Sender } from "../core/sender";
 
 export class MessageHandler {
   public static async Init() {
@@ -14,6 +15,14 @@ export class MessageHandler {
           ? MessageHelper.GetCommand(Config, message).trim()
           : "";
         const parameter = MessageHelper.GetParameter(Config, message).trim();
+        if (isCommand && MessageHelper.IsDMChannel(message)) {
+          Sender.Send(
+            message,
+            `Go me nasai!, I can't process commands in DM.`,
+            true
+          );
+          return;
+        }
         if (isCommand) {
           const command = new Command(cmdName, parameter);
           console.log(command);
